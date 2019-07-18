11a685c8a7510039d87c46ef591dc677211b43b1
zeinabb <zeinab.b1373@gmail.com>
2019-07-18 13:31:45 +0430

    Exercise2

--- day4-exercise/src/Exercises/UserCard/UserCard.js
+++ day4-exercise/src/Exercises/UserCard/UserCard.js
@@ -3,7 +3,33 @@ import families from '../../data.json';
 import UserSelfCard from './UserSelfCard';
 
 const UserCard = ({ user }) => {
-  return null;
+
+  // let [width, setWidth] = React.useState(document.body.clientWidth);
+
+  // React.useEffect(() => {
+  //   window.addEventListener("resize", () => {
+  //     setWidth(document.body.clientWidth);
+  //   });
+  // }, [document.body.clientWidth]);
+  return (
+    <div>
+      <UserSelfCard user={user} />
+
+      <div style={{ backgroundColor: 'lightcyan', width: "25%", margin: 16, padding: 16, borderRadius: 15, marginRight: "auto", marginLeft: "auto" }}>
+        <h4 style={{ margin: 0 }}> Married to </h4>
+        <a style={{ margin: 0, marginTop: 8 }} href={user.marriedTo} >{user.marriedTo} </a>
+      </div>
+
+
+      <div style={{ backgroundColor: 'lightcyan', width: "25%", margin: 16, padding: 16, borderRadius: 15, marginRight: "auto", marginLeft: "auto" }}>
+        <h4 style={{ margin: 0 }}> Children </h4>
+        <div style={{ flexDirection: "column" , display : "flex" }}>
+          {user.children.map(id => <a href={`#${id}`}> {id} </a>)}
+        </div>
+      </div>
+
+    </div>
+  )
 };
 
 export default UserCard;
--- day4-exercise/src/Exercises/UserCard/UserSelfCard.js
+++ day4-exercise/src/Exercises/UserCard/UserSelfCard.js
@@ -1,24 +1,32 @@
 import React from 'react';
 
+
 const UserSelfCard = ({ user }) => {
+  let [width, setWidth] = React.useState(document.body.clientWidth);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWidth(document.body.clientWidth);
+    });
+  }, [document.body.clientWidth]);
   console.log(user)
   return (
-    <div style = {{paddingTop : 16}}> 
-     <div style = {{backgroundColor : 'lightcyan' , width : "25%" , minWidth : 250, padding: 16, borderRadius : 15, marginRight : "auto", marginLeft : "auto" }} > 
-          <div style = {{flexDirection : "row", display : "flex", alignItems:'center'}}>
-              <img src = {user.image} style = {{width : 75 , height : 75, borderRadius : 50}}/>
+    <div style={{ paddingTop: 16 }}>
+      <div id={user.id} style={{ backgroundColor: 'lightcyan', width: "25%", padding: 16, borderRadius: 15, marginRight: "auto", marginLeft: "auto" }} >
+        <div style={{ flexDirection: width < 700 ? 'column' : 'row', display: "flex", alignItems: 'center' }}>
+          <img src={user.image} style={{ width: 75, height: 75, borderRadius: 50 }} />
 
-              <div style={{marginLeft: 8}}>
-                <h4 style = {{margin : 0}}> {user.name} </h4>
-                <h4 style = {{margin : 0, marginTop: 8}}> {user.surName} </h4>
-              </div>
+          <div style={{ marginLeft: 8, textAlign: width < 700 ? 'center' : 'left' }}>
+            <h4 style={{ margin: 0 }}> {user.name} </h4>
+            <h4 style={{ margin: 0, marginTop: 8 }}> {user.surName} </h4>
           </div>
+        </div>
 
-          <div style = {{marginTop: 8}}>
-      <span> {user.gender}.{user.age} </span> 
-          </div>
+        <div style={{ marginTop: 8, textAlign: width < 700 ? 'center' : 'left' }}>
+          <span> {user.gender}.{user.age} </span>
+        </div>
+      </div>
     </div>
-    </div> 
   );
 };
 export default UserSelfCard;
