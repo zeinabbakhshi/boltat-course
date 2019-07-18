296e4f8b0b96b86217784055fff9968a49c2f1b0
zeinabb <zeinab.b1373@gmail.com>
2019-07-18 12:16:47 +0430

    Hello

--- day4-exercise/src/Exercises/UserCard/UserSelfCard.js
+++ day4-exercise/src/Exercises/UserCard/UserSelfCard.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 const UserSelfCard = ({ user }) => {
-  return null;
+  console.log(user)
+  return (
+    <div style = {{paddingTop : 16}}> 
+     <div style = {{backgroundColor : 'lightcyan' , width : "25%" , minWidth : 250, padding: 16, borderRadius : 15, marginRight : "auto", marginLeft : "auto" }} > 
+          <div style = {{flexDirection : "row", display : "flex", alignItems:'center'}}>
+              <img src = {user.image} style = {{width : 75 , height : 75, borderRadius : 50}}/>
+
+              <div style={{marginLeft: 8}}>
+                <h4 style = {{margin : 0}}> {user.name} </h4>
+                <h4 style = {{margin : 0, marginTop: 8}}> {user.surName} </h4>
+              </div>
+          </div>
+
+          <div style = {{marginTop: 8}}>
+      <span> {user.gender}.{user.age} </span> 
+          </div>
+    </div>
+    </div> 
+  );
 };
 export default UserSelfCard;
